6d03d3cc52813478f6fbf74bc7cce953e7fef323
mapan1984 <mapan1984@outlook.com>
2020-09-23 19:07:43 +0800

    去除 PowerTab, PSColor, PsGet modules

--- profile.ps1
+++ profile.ps1
@@ -1,26 +1,19 @@
-﻿<#
-Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-#>
-
 ### Load Module Plug
-Import-Module PSColor
-#$poshGitModule = Get-Module posh-git -ListAvailable | Sort-Object Version -Descending | Select-Object -First 1
-#if ($poshGitModule) {
-#    $poshGitModule | Import-Module
-#}
-#elseif (Test-Path -LiteralPath ($modulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) (Join-Path src 'posh-git.psd1'))) {
-#    Import-Module $modulePath
-#}
-#else {
-#    throw "Failed to import posh-git."
-#}
-#Import-Module posh-git
-#Import-Module PowerTab -ArgumentList ".\PowerTabConfig.xml"
-#Import-Module PsGet
+Import-Module PSReadLine
+# Import-Module PSColor
+Import-Module posh-git
+# Import-Module PowerTab -ArgumentList ".\PowerTabConfig.xml"
+# Import-Module PsGet
 
 ### All alias
-Set-Alias -Name l -Value Get-ChildItem
-Set-Alias -Name vi -Value Code
+# Set-Alias -Name l -Value Get-ChildItem
+# Set-Alias -Name vi -Value Code
+#
+# Function CATHIS {Get-Content (Get-PSReadlineOption).HistorySavePath}
+# Set-Alias -Name his -Value CATHIS
+#
+# Function CD32 {Set-Location -Path C:\Windows\System32}
+# Set-Alias -Name Go -Value CD32
 
 ### Add user path
 $currentDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
@@ -37,7 +30,6 @@ $prp = new-object System.Security.Principal.WindowsPrincipal($wid)
 $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 $IsAdmin = $prp.IsInRole($adm)
 
-# Here we go
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
 
@@ -54,11 +46,8 @@ function global:prompt {
     write-Host " Ω " -nonewline -ForegroundColor Gray
     Write-Host ($pwd.ProviderPath) -nonewline -ForegroundColor Green
 
-    <# posh-git for git: show information if in git rep #>
-    $realLASTEXITCODE = $LASTEXITCODE
+    # posh-git status
     Write-VcsStatus
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    <# posh-git for git: end #>
 
     # Rightmost time display
     $currentY = [console]::CursorTop        # Save cursor position first
@@ -75,11 +64,28 @@ function global:prompt {
     return " "
 }
 
+
+### PSReadLineOption
 # 设置 EditMode
 Set-PSReadLineOption -EditMode Emacs
-
-<#
-Pop-Location
-
-Start-SshAgent -Quiet
-#>
+#
+Set-PSReadLineOption -Colors @{
+    Command             = "#e5c07b"
+    Number              = "#cdd4d4"
+    Member              = "#e06c75"
+    Operator            = "#e06c75"
+    Type                = "#78b6e9"
+    Variable            = "#78b6e9"
+    Parameter           = "#e06c75"  #命令行参数颜色
+    ContinuationPrompt  = "#e06c75"
+    Default             = "#cdd4d4"
+    Emphasis            = "#e06c75"
+    #Error
+    Selection           = "#cdd4d4"
+    Comment             = "#cdd4d4"
+    Keyword             = "#e06c75"
+    String              = "#78b6e9"
+}
+# Clipboard interaction is bound by default in Windows mode, but not Emacs mode.
+Set-PSReadLineKeyHandler -Key Ctrl+C -Function Copy
+Set-PSReadLineKeyHandler -Key Ctrl+v -Function Paste
